cfe1866b9de8b7a72caaa81e198dd38602fba229
sundaym <sunday2011s@gmail.com>
2023-01-31 09:26:40 +0800

    listagg() within group()

---
+++ 11g/listagg.sql
@@ -0,0 +1,30 @@
+/*********************************
+listagg examples
+*********************************/
+
+-- data
+with emp as (
+    select '20' deptno, 'John' ename from dual
+    union
+    select '20' deptno, 'Bob' ename from dual
+    union
+    select '20' deptno, 'Wick' ename from dual
+    union
+    select '30' deptno, 'Alex' ename from dual
+)
+select deptno, ename from emp
+
+-- example1 多行合并成一行 listagg() within group ()
+with emp as (
+    select '20' deptno, 'John' ename from dual
+    union
+    select '20' deptno, 'Bob' ename from dual
+    union
+    select '20' deptno, 'Wick' ename from dual
+    union
+    select '30' deptno, 'Alex' ename from dual
+)
+select deptno,
+       listagg(ename, ',') within group ( order by ename) ename
+from emp
+group by deptno

7f699d5f1ca32502ff8ef80dc42ca868624cde8e
sundaym <sunday2011s@gmail.com>
2023-02-01 16:05:55 +0800

    global temporary table

---
+++ 11g/temporary_table.sql
@@ -0,0 +1,23 @@
+/*********************************
+Oracle temporary table
+
+*********************************/
+
+-- 1. session
+
+create global temporary table temp_table_session
+(
+    col1 varchar2(100),
+    col2 varchar2(100),
+    col3 varchar2(100)
+)
+    on commit delete rows;
+
+-- 2. transaction
+create global temporary table temp_table_transaction
+(
+    col1 varchar2(100),
+    col2 varchar2(100),
+    col3 varchar2(100)
+)
+    on commit preserve rows;

31da3ad94b7d8445a5ec951dbdd14f309a209d81
sundaym <sunday2011s@gmail.com>
2023-07-18 11:06:10 +0800

    lengthb

--- common/time.sql
+++ common/time.sql
@@ -17,3 +17,15 @@ select to_char(sysdate, 'ww') from dual; -- 获取周数, 每年1月1日为第1�
 -- 两个时间段存在交集的情况比较多，但不存在交集的情况就比较少，所以排除不存在交集的情况，取反就能获得存在交集的结果
 -- 不存在交集的情况一个是 end < start_time, 一个是 start > end_time, end < start_time || start > end_time
 select * from test_table where not ((end < start_time) or (start > end_time));
+
+-- timestamp
+SELECT EXTRACT(YEAR FROM SYSTIMESTAMP) YEAR,
+       EXTRACT(MONTH FROM SYSTIMESTAMP) MONTH,
+       EXTRACT(DAY FROM SYSTIMESTAMP) DAY,
+       EXTRACT(MINUTE FROM SYSTIMESTAMP) MINUTE,
+       EXTRACT(SECOND FROM SYSTIMESTAMP) SECOND,
+       EXTRACT(TIMEZONE_HOUR FROM SYSTIMESTAMP) TH,
+       EXTRACT(TIMEZONE_MINUTE FROM SYSTIMESTAMP) TM,
+       EXTRACT(TIMEZONE_REGION FROM SYSTIMESTAMP) TR,
+       EXTRACT(TIMEZONE_ABBR FROM SYSTIMESTAMP) TA
+FROM DUAL
